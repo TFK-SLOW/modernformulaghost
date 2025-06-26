ecd665aafcf517e16d11566b97847b5f08ba5856
SLOW <kz.guira08@gmail.com>
2025-06-26 19:39:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -187,4 +187,13 @@ function resetPassword() {
     });
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+  });
+});
+
 
